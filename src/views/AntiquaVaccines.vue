9770ab8baf454d78a184754505e7e231d2a3630b
banaanihillo <banaanihillo@protonmail.ch>
2021-06-20 13:12:44 +0300

    Added vaccine details by producer

--- src/views/AntiquaVaccines.vue
+++ src/views/AntiquaVaccines.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-
+    <p>
+      {{antiqua.length}}
+    </p>
   </div>
 </template>
 
 <script>
+import antiqua from "../../resources/antiqua.json"
 export default {
-
+  data() {
+    return {
+      antiqua
+    }
+  }
 }
 </script>
 
--- src/views/SolarBuddhicaVaccines.vue
+++ src/views/SolarBuddhicaVaccines.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-
+    <p>
+      {{solarBuddhica.length}} Solar Buddhica vaccines available.
+    </p>
   </div>
 </template>
 
 <script>
+import solarBuddhica from "../../resources/solarBuddhica.json"
 export default {
-
+  data() {
+    return {
+      solarBuddhica
+    }
+  }
 }
 </script>
 
