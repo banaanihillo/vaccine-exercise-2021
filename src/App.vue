5326f3f82bb6448282484f048fb0d63d2f076f1e
banaanihillo <banaanihillo@protonmail.ch>
2021-06-20 12:18:03 +0300

    Added general vaccination information
    Added the total count of vaccinations,
    which is 7000, right,
    also added a fictional date for today,
    with which to observe recent vaccinations

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <main>
+  <div>
     <nav>
       <router-link to="/">Vaccinations</router-link>
       <router-link to="/antiqua">Antiqua</router-link>
       <router-link to="/solar-buddhica">Solar Buddhica</router-link>
       <router-link to="/zerpfy">Zerpfy</router-link>
     </nav>
-    <router-view />
-  </main>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <small>
+        It's {{today.slice(0, today.indexOf("T"))}} today.
+        (Not really, but for the purposes of this exercise.)
+      </small>
+    </footer>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      today: new Date("2021-04-12T11:10:06.473587Z").toISOString()
+    }
+  }
+}
+</script>
 
 <style>
 body {
--- src/views/Vaccinations.vue
+++ src/views/Vaccinations.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-
+    <p>
+      Total of {{vaccines.count}} vaccinations administered.
+    </p>
+    <p>
+      {{vaccines.lastMonth.length}} vaccinations in the last 30 days.
+    </p>
   </div>
 </template>
 
 <script>
+import vaccinations from "../../resources/vaccinations.json"
 export default {
-
+  data() {
+    return {
+      vaccines: {
+        count: vaccinations.length,
+        lastMonth: vaccinations.filter((vaccination) => {
+          return (
+            new Date(vaccination.vaccinationDate)
+            >= new Date("2021-03-12")
+          )
+        })
+      }
+    }
+  }
 }
 </script>
 
